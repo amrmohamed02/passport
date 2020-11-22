19abc5c1161e27416f06c323b4be391cd7321f09
unknown <amr.delaa@gmail.com>
2020-11-22 13:47:24 +0200

    edit login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\RegisterRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
@@ -14,20 +15,18 @@ class UserController extends Controller
     public function register(RegisterRequest $request)
     {
         try {
-
             //** Get requested data by RegisterRequest */
             $register_data = $request->validated();
             $user = new User();
             $user->name = $register_data['name'];
             $user->email = $register_data['email'];
             $user->password = bcrypt($register_data['password']);
-            $user->save();
-
             # Create a token if not found
             $token = $user->revoke_tokens();
-
+            $user->remember_token= $token;
+            $user->save();
             //** Get User Object */
-            $success_data = ['student' => (new UserLogin($user))->set_token($token)];
+            $success_data = ['users' => (new UserLogin($user))->set_token($token)];
             $this->response = $this->show_success($this->response, 'Registered successfully!', $success_data);
         } catch (\Exception $e) {
             $this->response = $this->show_error($this->response, $e);
@@ -38,13 +37,25 @@ class UserController extends Controller
 
     }
 
-    public
-    function login(Request $request)
+    public function login(Request $request)
     {
-        $email = $request->input('email');
-        $password = $request->input('password');
-        $user = User::where('email', $email)->where('password', $password)->get();
-        return UserLogin::collection($user);
+        try{
+            
+            $email = $request->input('email');
+            $password = $request->input('password');
+            $user = User::where('email', $email)->first();
+            if ($user){
+                if(Hash::check($password, $user->password)){
+                    $this->response = $this->show_success($this->response, 'login successfully!',new UserLogin($user));
+                } 
+            }
+        }
+        catch (\Exception $e) {
+            $this->response = $this->show_error($this->response, $e);
+        }
+        finally {
+            return response()->json($this->response, 200);
+        }
 
     }
 }
